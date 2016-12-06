e8bd01ebe569273677787cbcc353b8b4cda4c849
alvarezsineiro <alvarez.sineiro@hotmail.com.ar>
2016-12-06 00:01:28 -0300

    Trabajo Practico N°4

---
+++ Navegador TP-4 - AlumnoV2/Archivos/Texto.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        private string _ruta;
+
+        public Texto(string archivo)
+        {
+            this._ruta = archivo;
+        }
+
+        /// <summary>
+        /// Guarda los datos indicados en un archivo
+        /// </summary>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        public bool guardar(string datos)
+        {
+            bool flag = false;
+            try
+            {
+                using (StreamWriter writer = new StreamWriter (this._ruta,true))
+                {
+                    writer.WriteLine(datos);
+                    writer.Close();
+                    flag = true;
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception();
+            }
+            return flag;
+            
+        }
+
+        /// <summary>
+        /// Lee los datos indicados de un archivo
+        /// </summary>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        public bool leer(out List<string> datos)
+        {
+            bool flag = false;
+            datos = new List<string>();
+            try
+            {
+                using (StreamReader reader = new StreamReader(this._ruta))
+                {
+                    while (reader.EndOfStream==false)
+                    {
+                        datos.Add(reader.ReadLine());
+                        flag = true;
+                    }                    
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception();
+            }
+            return flag;
+        }
+    }
+}
---
+++ Navegador TP-4 - AlumnoV2/Hilo/Descargador.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+
+        public Descargador(Uri direccion)
+        {
+            this.html = "";
+            this.direccion = direccion;
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += this.WebClientDownloadProgressChanged;
+                cliente.DownloadStringCompleted += this.WebClientDownloadCompleted;
+
+                cliente.DownloadStringAsync(direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        /// <summary>
+        /// Delegado del evento descargando
+        /// </summary>
+        /// <param name="Progreso"></param>
+        public delegate void EventDescargando(int Progreso);
+
+        /// <summary>
+        /// Evento del tipo del delegado
+        /// </summary>
+        public event EventDescargando Descargando;
+
+        /// <summary>
+        /// Genera un evento
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.Descargando(e.ProgressPercentage);
+        }
+
+        /// <summary>
+        ///  Delegado del evento descargacompleta
+        /// </summary>
+        /// <param name="Terminado"></param>
+        public delegate void EventCompletado(string Terminado);
+        
+        /// <summary>
+        /// Evento del tipo del delegado
+        /// </summary>
+        public event EventCompletado DescargaCompleta;
+
+        /// <summary>
+        /// Genera un evento
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            this.DescargaCompleta(e.Result);
+        }
+    }
+}
